073d2a7967cdf91d71ffea8887f2ef05228164ff
Michelle Austin <michelle.austin@itv.com>
2021-03-14 10:26:32 +0000

    updates to AIC clients, models, test

--- src/main/scala/com/michelle/rijksdata/Clients/AICClient.scala
+++ src/main/scala/com/michelle/rijksdata/Clients/AICClient.scala
@@ -3,29 +3,55 @@ package com.michelle.rijksdata.Clients
 import cats.effect.IO
 import cats.implicits.catsSyntaxApplicativeId
 import com.michelle.rijksdata.EffectfulLogging
-import com.michelle.rijksdata.Models.AICSearchResponse
+import com.michelle.rijksdata.Models.{
+  AICItemResult,
+  AICSearchResponse,
+  ItemConfig,
+  ItemData
+}
 import org.http4s.Method.GET
 import org.http4s.{EntityDecoder, Request, Uri}
 import org.http4s.circe.jsonOf
 import org.http4s.client.Client
+import cats.implicits._
 
 trait AICClient {
- def getSearchResult: IO[AICSearchResponse]
+  def getSearchResult: IO[AICSearchResponse]
+  def getItemResult(
+      aicSearchResponse: AICSearchResponse): IO[List[AICItemResult]]
 }
 
 object AICClient extends EffectfulLogging {
 
-  implicit val AICSearchResponseEntityDecoder: EntityDecoder[IO, AICSearchResponse] = jsonOf
+  implicit val AICSearchResponseEntityDecoder
+    : EntityDecoder[IO, AICSearchResponse] = jsonOf
 
   def apply(client: Client[IO], baseUri: Uri): AICClient = {
     new AICClient {
-     override def getSearchResult: IO[AICSearchResponse] = {
-      val url = baseUri / "search" / "artworks"
-       val urlWithQuery = url.withQueryParam("q", "woodcut")
-       client.run(Request[IO](GET, urlWithQuery)).use {
-        case response if response.status.isSuccess => response.as[AICSearchResponse]
-        case _ => AICSearchResponse(List.empty).pure[IO]}
-     }
+      override def getSearchResult: IO[AICSearchResponse] = {
+        val url = baseUri / "search" / "artworks"
+        val urlWithQuery = url.withQueryParam("q", "woodcut")
+        client.run(Request[IO](GET, urlWithQuery)).use {
+          case response if response.status.isSuccess =>
+            response.as[AICSearchResponse]
+          case _ => AICSearchResponse(List.empty).pure[IO]
+        }
+      }
+
+      implicit val AICItemResultEntityDecoder
+        : EntityDecoder[IO, AICItemResult] = jsonOf
+
+      override def getItemResult(
+          aicSearchResponse: AICSearchResponse): IO[List[AICItemResult]] = {
+        val urls = aicSearchResponse.data.map(l => Uri(path = l.api_link))
+        urls.map(u =>
+          client.run(Request[IO](GET, u)).use {
+            case response if response.status.isSuccess =>
+              response.as[AICItemResult]
+            case _ =>
+              AICItemResult(ItemData("", "", "", ""), ItemConfig("")).pure[IO]
+        })
+      }.sequence
     }
   }
 }
--- src/main/scala/com/michelle/rijksdata/Models/AICModels.scala
+++ src/main/scala/com/michelle/rijksdata/Models/AICModels.scala
@@ -2,11 +2,32 @@ package com.michelle.rijksdata.Models
 
 import io.circe.Decoder
 import io.circe.generic.semiauto.deriveDecoder
+import org.http4s.blaze.http.Url
 
 case class AICSearchResponse(data: List[ApiLink])
 case class ApiLink(api_link: String)
 
 object AICSearchResponse {
-  implicit val AICSearchResponseDecoder: Decoder[AICSearchResponse] = deriveDecoder[AICSearchResponse]
-  implicit val apiLinkDecoder: Decoder[ApiLink] = deriveDecoder[ApiLink]
+  implicit val AICSearchResponseDecoder: Decoder[AICSearchResponse] =
+    deriveDecoder
+  implicit val apiLinkDecoder: Decoder[ApiLink] = deriveDecoder
+}
+
+case class AICItemResult(data: ItemData, config: ItemConfig)
+case class ItemData(title: String,
+                    artist_display: String,
+                    place_of_origin: String,
+                    image_id: String)
+case class ItemConfig(iiif_url: String)
+
+object AICItemResult {
+  implicit val AICItemResultDecoder: Decoder[AICItemResult] = deriveDecoder
+  implicit val itemDataDecoder: Decoder[ItemData] = deriveDecoder
+  implicit val itemConfigDecoder: Decoder[ItemConfig] = deriveDecoder
+}
+
+case class AIC(itemResult: AICItemResult){
+  def createImageLink = {
+    itemResult.config.iiif_url.concat("/").concat(itemResult.data.image_id).concat("/full/843,/0/default.jpg")
+  }
 }
